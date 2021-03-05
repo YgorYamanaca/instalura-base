03f3522f788f315c4e25c52b5944886c988f2b73
Ygor <ygor_sy@hotmail.com>
2021-03-05 00:42:13 -0300

    teste

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,7 +8,7 @@ import GlobalStyle from '../src/components/GlobalStyle';
 const App: React.FC<AppProps> = ({ Component, pageProps }) => (
   <>
     <Head>
-      <title>Projeto base - instalura</title                     >
+      <title>Projeto base - instalura</title                               >
       <link rel="preconnect" href="https://fonts.gstatic.com" />
       <link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet" />
     </Head>
